9c9adfc5513d6c6c397e45ebfd1a3c23e3958c95
teja1625 <suryateja2612@gmail.com>
2024-09-23 08:39:57 +0530

    first commit

---
+++ convex/auth.config.ts
@@ -0,0 +1,12 @@
+//https://crisp-macaque-73.clerk.accounts.dev
+
+const authConfig = {
+  providers: [
+    {
+      domain: "https://crisp-macaque-73.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
+
+export default authConfig;
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lovely-flamingo-139.convex.cloud",
+      },
+      {
+        protocol: "https",
+        hostname: "optimistic-crab-974.convex.cloud",
+      },
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
